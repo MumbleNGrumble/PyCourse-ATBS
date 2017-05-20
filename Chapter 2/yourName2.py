cbafcb919ff63c315bd3568a31134a8d20e2ff86
MumbleNGrumble <le.daniel90@gmail.com>
2017-05-20 15:12:52 -0700

    Created yourName2.py

---
+++ Chapter 2/yourName2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 20 15:09:41 2017
+
+@author: Daniel
+"""
+
+while True:
+    print('Please type your name.')
+    name = input()
+    if name == 'your name':
+        break
+print('Thank you!')
