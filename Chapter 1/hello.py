c9bcc65b37a4b84d4c0c052ce2d73bbe02f9f2c9
MumbleNGrumble <le.daniel90@gmail.com>
2017-05-20 12:50:22 -0700

    Changed indentations for readability

--- Chapter 1/hello.py
+++ Chapter 1/hello.py
@@ -8,9 +8,9 @@ Created on Sat May 20 12:08:41 2017
 # This program says hello and asks for my name.
 
 print('Hello World!')
+
 print('What is your name?') # ask for their name
 myName = input()
-
 print('It is good to meet you, ' + myName)
 print('The length of your name is:')
 print(len(myName))
